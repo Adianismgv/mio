f311e579dd55e446d4484fdc8b33ce06e16e471a
nelsonjpr <socbrazil666@gmail.com>
2025-11-23 22:17:56 -0500

    Use RegExp for catch-all route to bypass path-to-regexp string parsing issues

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.listen(port, () => {
 });
 
 // Handle React routing, return all requests to React app
-app.get('/:pathMatch(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
