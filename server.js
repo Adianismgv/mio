65ff0c6aee17849c100bbb535558c9796c026032
nelsonjpr <socbrazil666@gmail.com>
2025-11-23 22:11:05 -0500

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.listen(port, () => {
 });
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
